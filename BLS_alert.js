e097aec8ca531d97e91a61ec07eb5a1dd83519ed
Faycalharmas <156510408+Faycalharmas@users.noreply.github.com>
2024-01-13 02:16:41 -0800

    Update BLS_alert.js

--- BLS_alert.js
+++ BLS_alert.js
@@ -1,10 +1,10 @@
 // ==UserScript==
 // @name         BLS alert
 // @namespace    http://tampermonkey.net/
-// @version      2.0.3
+// @version      3.0
 // @description  bls aler  P|R|C Clean Refresh Alarm Notif
 // @author       @Itsmaarouf
-// @match        *://*.blsspainmorocco.com/*
+// @match        *://*.algeria.blsspainvisa.com/*
 // @grant        none
 // ==/UserScript==
 
